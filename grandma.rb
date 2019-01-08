ce67bbaa3df9cda70222f02825caef6542a29c66
Mackenzie Moore <macmoore@ucdavis.edu>
2019-01-08 04:33:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(words)
   if words == 'I LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN!'
+  return 'I LOVE YOU TOO PUMPKIN!'
   elsif words == words.upcase
-    return 'NO, NOT SINCE 1938!'
+  return 'NO, NOT SINCE 1938!'
   else 
-    return 'HUH?! SPEAK UP, SONNY!'
+  return 'HUH?! SPEAK UP, SONNY!'
 end
